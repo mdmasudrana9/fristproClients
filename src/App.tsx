91cbc81a7a180b4db43a0eb3308c3fb33daaf5c3
masud-rana9 <rana61960@gmail.com>
2025-04-17 00:19:26 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "./component/layout/MainLayout";
+
+const App = () => {
+  return (
+    <div className="">
+      <MainLayout />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/layout/AdminLayout.tsx
@@ -0,0 +1,5 @@
+const AdminLayout = () => {
+  return <div className="">This is AdminLayout Components</div>;
+};
+
+export default AdminLayout;
